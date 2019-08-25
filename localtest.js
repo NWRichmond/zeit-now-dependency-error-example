5ed587550c5079545a53c941f0ba3094287a762e
Nick Richmond <nick@pop-os.localdomain>
2019-08-24 23:32:23 -0400

    Define core function and associated local test

---
+++ localtest.js
@@ -0,0 +1,24 @@
+const getThoseLyrics = require('./src/getLyrics');
+const http = require("http");
+require('dotenv').config();
+
+const hostname = "127.0.0.1";
+const port = 5555;
+
+const server = http.createServer(async (req, res) => {
+  try {
+    const artistAndSong = 'frankie-laine-rawhide';
+    const lyrics = await getThoseLyrics(artistAndSong, true);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(lyrics);
+    console.log('\nGot the lyrics! 🎵\n'); 
+  } catch(err) {
+    errorMessage = `Error getting lyrics:\n\n${err.message}\n\n`;
+    console.log(errorMessage);
+    res.end(errorMessage);
+  }
+});
+
+server.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ src/getLyrics.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer-extra');
+
+module.exports = async function getLyrics(artistAndSong, stealthEnabled = false) {
+  try {
+    if (stealthEnabled) {
+      console.log(`Enabling 'puppeteer-extra-plugin-stealth'...`)
+      const pluginStealth = require('puppeteer-extra-plugin-stealth');
+      puppeteer.use(pluginStealth());
+    }
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_API_KEY}`,
+    });
+    const url = `https://genius.com/${artistAndSong}-lyrics`;
+    const page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: 'networkidle0',
+    });
+    const selector = '.lyrics'
+    await page.waitForSelector(selector);
+    const textElement = await page.$(selector);
+    const textHandle = await textElement.getProperty('innerText');
+    const text = await textHandle.jsonValue();
+    await browser.close();
+    return text;
+  } catch(err) {
+    await browser.close();
+    const errorMessage = `Problem getting lyrics from Genius: \n${err.message}`;
+    console.log(errorMessage);
+    throw errorMessage;
+  }
+}
